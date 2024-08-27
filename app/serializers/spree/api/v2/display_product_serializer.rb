f46b6128e149ce6fd73813b2468f53f3f7109764
Prakash <poudelprakash@users.noreply.github.com>
2024-08-27 19:36:53 +0545

    Update display_product_serializer.rb
    Add product field

--- app/serializers/spree/api/v2/display_product_serializer.rb
+++ app/serializers/spree/api/v2/display_product_serializer.rb
@@ -3,6 +3,8 @@ module Spree
     module V2
       class DisplayProductSerializer < BaseSerializer
         attributes :id, :product_id, :display_id, :qr_code_url, :video_url
+
+         has_one :product
       end
     end
   end
