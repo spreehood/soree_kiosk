7f189dcf25ed8b53aa9cfb171511b2804f7299f5
Mausam Pun <sonicmausam@gmail.com>
2024-08-27 10:00:01 +0545

    feature/add-products-to-display : Add callback to update the products_array when a new display product is added

--- app/models/spree/display_product.rb
+++ app/models/spree/display_product.rb
@@ -5,11 +5,20 @@ module Spree
     belongs_to :display
     belongs_to :product
 
+    after_save :append_to_products_array
+
     validates :display, presence: true
 
     scope :ordered_by_products_array, ->(products_array) {
       order_clause = products_array.map.with_index { |id, index| "id = #{id} DESC" }.join(", ")
       where(id: products_array).order(Arel.sql(order_clause))
     }
+
+    private
+
+    def append_to_products_array
+      display.products_array.append(id) unless display.products_array.include?(id)
+      display.save!
+    end
   end
 end
